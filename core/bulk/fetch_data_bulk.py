525c92f593bc44e89a8504052e6e30db37f7ec5e
jinjernot <norindes@gmail.com>
2025-10-17 15:31:48 -0600

    hotfix

--- core/bulk/fetch_data_bulk.py
+++ core/bulk/fetch_data_bulk.py
@@ -46,7 +46,7 @@ def fetch_and_save_data(target_date=None):
             # These are your other 6 required calls
             executor.submit(fetch_data, BOUNCEBACK_OData_ENDPOINT, "bouncebacks_odata.json", extra_params={"$filter": f"bounceBackDateHour ge {start_str} and bounceBackDateHour lt {end_str_bounceback}"}): "bouncebacks",
             executor.submit(fetch_data, CLICKTHROUGH_ENDPOINT, "email_clickthrough.json", extra_params={"$filter": f"clickDateHour ge {start_str} and clickDateHour lt {end_str_engagement}"}): "email_clickthroughs",
-            executor.submit(fetch_data, EMAIL_OPEN_ENDPOINT, "email_open.json", extra_params={"$filter": f"openDateHour ge {start_str} and clickDateHour lt {end_str_engagement}"}): "email_opens",
+            executor.submit(fetch_data, EMAIL_OPEN_ENDPOINT, "email_open.json", extra_params={"$filter": f"openDateHour ge {start_str} and openDateHour lt {end_str_engagement}"}): "email_opens",
             executor.submit(fetch_data, CAMPAIGN_ANALYSIS_ENDPOINT, "campaign.json"): "campaign_analysis",
             executor.submit(fetch_data, CAMPAIGN_USERS_ENDPOINT, "campaign_users.json"): "campaign_users",
             executor.submit(fetch_data, EMAIL_ASSET_ENDPOINT, "email_asset.json"): "email_asset_data"
